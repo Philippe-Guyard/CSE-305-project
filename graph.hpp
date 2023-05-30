67cbc5f69ef848a23cf1bdf86ac6e35159c25264
pylyrka <avadakidabra.magic@gmail.com>
2023-05-30 23:15:32 +0200

    not working piece of shit

--- graph.hpp
+++ graph.hpp
@@ -1,59 +1,143 @@
 #pragma once
-#include <vector>
+#include <functional>
+#include <iostream>
 #include <random>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 using vertex_t = int;
 using edge_weight = double;
-
-// Standard definition of a directed graph with non-negative real edge weights
-// Store edges as adjacency lists
-struct Edge
-{
+struct Edge {
     vertex_t to;
     edge_weight weight;
 };
 
-class Graph
-{
+struct UEdge {
+    vertex_t from, to;
+    bool operator==(const UEdge &other) const {
+        return from == other.from && to == other.to;
+    }
+};
+
+struct DEdge {
+    vertex_t from, to;
+    edge_weight weight;
+    bool operator==(const DEdge &other) const {
+        return from == other.from && to == other.to && weight == other.weight;
+    }
+    bool operator<(const DEdge &other) const {
+        return from <= other.from || to <= other.to;
+    }
+};
+
+struct TEdge {
+    vertex_t from, to;
+    edge_weight weight;
+    int b;
+    bool operator==(const TEdge &other) const {
+        return from == other.from && to == other.to && weight == other.weight && b == other.b;
+    }
+};
+
+namespace std {
+    template <>
+    struct hash<UEdge> {
+        std::size_t operator()(const UEdge &edge) const {
+            std::size_t h1 = std::hash<vertex_t>()(edge.from);
+            std::size_t h2 = std::hash<vertex_t>()(edge.to);
+            return h1 ^ h2;
+        }
+    };
+    template <>
+    struct hash<DEdge> {
+        std::size_t operator()(const DEdge &edge) const {
+            std::size_t h1 = std::hash<vertex_t>()(edge.from);
+            std::size_t h2 = std::hash<vertex_t>()(edge.to);
+            std::size_t h3 = std::hash<edge_weight>()(edge.weight);
+            return h1 ^ h2 ^ h3;
+        }
+    };
+    template <>
+    struct hash<TEdge> {
+        std::size_t operator()(const TEdge &edge) const {
+            std::size_t h1 = std::hash<vertex_t>()(edge.from);
+            std::size_t h2 = std::hash<vertex_t>()(edge.to);
+            std::size_t h3 = std::hash<edge_weight>()(edge.weight);
+            std::size_t h4 = std::hash<int>()(edge.b);
+            return h1 ^ h2 ^ h3 ^ h4;
+        }
+    };
+}
+
+using edge_block = std::vector<DEdge>;
+
+// Standard definition of a directed graph with non-negative real edge weights
+// Store edges as adjacency lists
+
+template <typename Key, typename Value>
+class HashArray {
+private:
+    std::unordered_map<Key, Value> dict;
+    Value defaultValue;
+
+public:
+    HashArray(const Value &defaultValue)
+        : defaultValue(defaultValue), dict({}) {}
+
+    void insert(const Key &key, const Value &value) {
+        dict[key] = value;
+    }
+
+    Value get(const Key &key) const {
+        auto it = dict.find(key);
+        if (it != dict.end()) {
+            return it->second;
+        }
+        return defaultValue;
+    }
+};
+
+class Graph {
 private:
     std::vector<std::vector<Edge>> adjacency_lists;
+
+    // TODO: Normally, I think we should work directly with blocked_adjacency_lists
+    // but since Philippe wrote this code and I dont want to break anything, I will
+    // define a separate attribute and explicitely convert one to the other.
+    // Philippe can then integrate it directly if he wants to.
+
     // # of vertices, # of edges
     int n, m;
 
 public:
     Graph(int num_vertices = 0) : n(num_vertices), adjacency_lists(num_vertices), m(0) {}
 
-    void add_edge(vertex_t from, vertex_t to, edge_weight weight)
-    {
+    void add_edge(vertex_t from, vertex_t to, edge_weight weight) {
         adjacency_lists[from].push_back({to, weight});
         ++m;
     }
 
-    int num_vertices() const
-    {
+    int num_vertices() const {
         return adjacency_lists.size();
     }
 
-    const std::vector<Edge> &edges_from(vertex_t v) const
-    {
+    const std::vector<Edge> &edges_from(vertex_t v) const {
         return adjacency_lists[v];
     }
 
-    friend std::ostream &operator<<(std::ostream &os, const Graph &g)
-    {
+    friend std::ostream &operator<<(std::ostream &os, const Graph &g) {
         os << g.n << " " << g.m << std::endl;
-        for (vertex_t v = 0; v < g.n; ++v)
-        {
-            for (auto &e : g.edges_from(v))
-            {
+        for (vertex_t v = 0; v < g.n; ++v) {
+            for (auto &e : g.edges_from(v)) {
                 os << v << " " << e.to << " " << e.weight << std::endl;
             }
         }
         return os;
     }
 
-    friend std::istream &operator>>(std::istream &in, Graph &g)
-    {
+    friend std::istream &operator>>(std::istream &in, Graph &g) {
         int new_n, new_m;
         in >> new_n >> new_m;
 
@@ -64,8 +148,7 @@ public:
 
         vertex_t src, dest;
         edge_weight w;
-        for (int i = 0; i < new_m; ++i)
-        {
+        for (int i = 0; i < new_m; ++i) {
             in >> src >> dest >> w;
             std::cout << g.n << std::endl;
             std::cout << src << " " << dest << " " << w << std::endl;
@@ -77,4 +160,46 @@ public:
 
         return in;
     }
+
+    edge_weight get_delta_0() const {
+        edge_weight delta_0 = std::numeric_limits<edge_weight>::infinity();
+
+        // can be made parallel
+        for (vertex_t v = 0; v < n; ++v) {
+            for (auto &e : edges_from(v)) {
+                if (e.weight < delta_0) {
+                    delta_0 = e.weight;
+                }
+            }
+        }
+        return delta_0;
+    }
+
+    std::vector<std::vector<edge_block>> gen_blocked_adjacency_lists() const {
+        edge_weight delta_0 = get_delta_0();
+
+        // now for each vertex v, we want to sort the edges coming out of v
+        // into blocks. A block j contains all edges e such that
+        // delta_0 * 2^j <= e.weight < delta_0 * 2^(j+1)
+
+        std::vector<std::vector<edge_block>> blocked_adjacency_lists(n);
+        for (vertex_t v = 0; v < n; ++v) {
+            for (auto &e : edges_from(v)) {
+                edge_weight lower_bound = delta_0;
+                int block_index = 0;
+
+                while (e.weight >= lower_bound * 2) {
+                    lower_bound *= 2;
+                    block_index++;
+                }
+
+                // If there is not enough space, we should extend the vector and potentially add some empty blocks
+                while (blocked_adjacency_lists[v].size() <= block_index) {
+                    edge_block new_block({});
+                    blocked_adjacency_lists[v].push_back(new_block);
+                }
+                blocked_adjacency_lists[v][block_index].push_back(DEdge({v, e.to, e.weight}));
+            }
+        }
+    };
 };
--- main.cpp
+++ main.cpp
@@ -1,24 +1,22 @@
-#include <iostream>
-#include <vector>
-#include <limits>
-#include <unordered_set>
 #include <fstream>
-#include <thread>
+#include <iostream>
+#include <limits>
 #include <optional>
+#include <thread>
+#include <unordered_set>
+#include <vector>
 
+#include "dijkstra.cpp"
 #include "graph.hpp"
 #include "test.cpp"
-#include "dijkstra.cpp"
 
 using dist_vector = std::vector<double>;
 
-class DeltaSteppingSolver
-{
+class DeltaSteppingSolver {
     using bucket_t = std::unordered_set<vertex_t>;
 
 private:
-    enum edge_type
-    {
+    enum edge_type {
         light,
         heavy
     };
@@ -29,13 +27,11 @@ private:
     const Graph &graph;
     double delta;
 
-    void relax(Edge e)
-    {
+    void relax(Edge e) {
         double x = e.weight;
         vertex_t w = e.to;
 
-        if (x < distances[w])
-        {
+        if (x < distances[w]) {
             size_t src_bucket_index = (size_t)(distances[w] / delta);
             size_t dest_bucket_index = (size_t)(x / delta);
             buckets[src_bucket_index].erase(w);
@@ -44,39 +40,28 @@ private:
         }
     }
 
-    std::optional<int> first_non_empty_bucket()
-    {
-        for (size_t i = 0; i < buckets.size(); i++)
-        {
-            if (!buckets[i].empty())
-            {
+    std::optional<int> first_non_empty_bucket() {
+        for (size_t i = 0; i < buckets.size(); i++) {
+            if (!buckets[i].empty()) {
                 return i;
             }
         }
         return std::nullopt;
     }
 
-    bool edge_is_kind(const Edge &e, edge_type kind) const
-    {
-        if (kind == light)
-        {
+    bool edge_is_kind(const Edge &e, edge_type kind) const {
+        if (kind == light) {
             return e.weight <= delta;
-        }
-        else
-        {
+        } else {
             return e.weight > delta;
         }
     }
 
-    std::vector<Edge> find_requests(const bucket_t &R, edge_type kind) const
-    {
+    std::vector<Edge> find_requests(const bucket_t &R, edge_type kind) const {
         std::vector<Edge> res = {};
-        for (vertex_t v : R)
-        {
-            for (const Edge &e : graph.edges_from(v))
-            {
-                if (edge_is_kind(e, kind))
-                {
+        for (vertex_t v : R) {
+            for (const Edge &e : graph.edges_from(v)) {
+                if (edge_is_kind(e, kind)) {
                     res.push_back({e.to, e.weight + distances[v]});
                 }
             }
@@ -85,14 +70,10 @@ private:
         return res;
     }
 
-    static void find_requests_thread(DeltaSteppingSolver *sol, const std::vector<vertex_t> &R_vec, size_t start, size_t end, std::vector<Edge> &result)
-    {
-        for (size_t j = start; j < end; ++j)
-        {
-            for (Edge e : sol->graph.edges_from(R_vec[j]))
-            {
-                if (sol->edge_is_kind(e, sol->light))
-                {
+    static void find_requests_thread(DeltaSteppingSolver *sol, const std::vector<vertex_t> &R_vec, size_t start, size_t end, std::vector<Edge> &result) {
+        for (size_t j = start; j < end; ++j) {
+            for (Edge e : sol->graph.edges_from(R_vec[j])) {
+                if (sol->edge_is_kind(e, sol->light)) {
                     result.push_back({e.to, e.weight + sol->distances[R_vec[j]]});
                 }
             }
@@ -101,38 +82,31 @@ private:
 
     // NOTE: For the parallel version we need random index access, so we make R a vector instead of a set
     // This is the improvement from section 4 of the paper
-    std::vector<Edge> find_requests_paralle(const bucket_t &R, edge_type kind, size_t num_threads)
-    {
+    std::vector<Edge> find_requests_parallel(const bucket_t &R, edge_type kind, size_t num_threads) {
         size_t chunk_size = R.size() / num_threads;
         // Another copy... Slow
         std::vector<vertex_t> R_vec(R.begin(), R.end());
         std::vector<std::vector<Edge>> results(num_threads - 1);
         std::vector<std::thread> threads(num_threads - 1);
-        for (size_t i = 0; i < num_threads - 1; i++)
-        {
+        for (size_t i = 0; i < num_threads - 1; i++) {
             threads[i] = std::thread(find_requests_thread, this, std::ref(R_vec), i * chunk_size, (i + 1) * chunk_size, std::ref(results[i]));
         }
         size_t last_chunk_start = (num_threads - 1) * chunk_size;
         std::vector<Edge> res;
-        for (size_t j = last_chunk_start; j < R.size(); ++j)
-        {
-            for (Edge e : graph.edges_from(R_vec[j]))
-            {
-                if (edge_is_kind(e, kind))
-                {
+        for (size_t j = last_chunk_start; j < R.size(); ++j) {
+            for (Edge e : graph.edges_from(R_vec[j])) {
+                if (edge_is_kind(e, kind)) {
                     res.push_back({e.to, e.weight + distances[R_vec[j]]});
                 }
             }
         }
 
-        for (size_t i = 0; i < num_threads - 1; i++)
-        {
+        for (size_t i = 0; i < num_threads - 1; i++) {
             threads[i].join();
         }
 
         // NOTE: This combining might be quite slow. Is there a faster way to parallelize?
-        for (size_t i = 0; i < num_threads - 1; i++)
-        {
+        for (size_t i = 0; i < num_threads - 1; i++) {
             if (results[i].size() > 0)
                 res.insert(res.end(), results[i].begin(), results[i].end());
         }
@@ -140,10 +114,8 @@ private:
         return res;
     }
 
-    void relax_requests(const std::vector<Edge> &requests)
-    {
-        for (Edge e : requests)
-        {
+    void relax_requests(const std::vector<Edge> &requests) {
+        for (Edge e : requests) {
             relax(e);
         }
     }
@@ -152,25 +124,21 @@ public:
     // Idk why I do it like this, architecture is weird...
     DeltaSteppingSolver(const Graph &g) : graph(g) {}
 
-    dist_vector solve(vertex_t source, double delta)
-    {
+    dist_vector solve(vertex_t source, double delta) {
         // Implement the sequential delta-stepping algorithm as described in section 2 of
         // https://reader.elsevier.com/reader/sd/pii/S0196677403000762?token=DBD927418ED5D4C911A1BF7217666F8DFE7446018FE2D1892A519D20FADCBE4A95A45FB4E44FD74C8BFD946BEE125078&originRegion=eu-west-1&originCreation=20230506110955
         this->delta = delta;
         distances = dist_vector(graph.num_vertices(), std::numeric_limits<double>::infinity());
         buckets = std::vector<bucket_t>(MAX_BUCKETS);
         relax({source, 0});
-        while (true)
-        {
+        while (true) {
             auto opt_i = first_non_empty_bucket();
-            if (!opt_i.has_value())
-            {
+            if (!opt_i.has_value()) {
                 break;
             }
             int i = opt_i.value();
             bucket_t R;
-            while (!buckets[i].empty())
-            {
+            while (!buckets[i].empty()) {
                 auto requests = find_requests(buckets[i], light);
                 R.insert(buckets[i].begin(), buckets[i].end());
                 buckets[i].clear();
@@ -184,38 +152,131 @@ public:
     }
 
     // Same as above, but use the improvement from section 4 for faster requests findings
-    dist_vector solve_parallel_simple(vertex_t source, double delta, size_t num_threads)
-    {
+    dist_vector solve_parallel_simple(vertex_t source, double delta, size_t num_threads) {
         this->delta = delta;
         distances = dist_vector(graph.num_vertices(), std::numeric_limits<double>::infinity());
         buckets = std::vector<bucket_t>(MAX_BUCKETS);
         relax({source, 0});
-        while (true)
-        {
+        while (true) {
             auto opt_i = first_non_empty_bucket();
-            if (!opt_i.has_value())
-            {
+            if (!opt_i.has_value()) {
                 break;
             }
             int i = opt_i.value();
             bucket_t R;
-            while (!buckets[i].empty())
-            {
-                auto requests = find_requests_paralle(buckets[i], light, num_threads);
+            while (!buckets[i].empty()) {
+                auto requests = find_requests_parallel(buckets[i], light, num_threads);
                 R.insert(buckets[i].begin(), buckets[i].end());
                 buckets[i].clear();
                 relax_requests(requests);
             }
-            auto requests = find_requests_paralle(R, heavy, num_threads);
+            auto requests = find_requests_parallel(R, heavy, num_threads);
             relax_requests(requests);
         }
 
         return distances;
     }
+
+    edge_weight find_delta() {
+        auto blocked_adjacency_lists = graph.gen_blocked_adjacency_lists();
+
+        edge_weight inf = std::numeric_limits<double>::infinity();
+        HashArray<UEdge, edge_weight> found(inf);
+        std::unordered_set<DEdge> Q({}), Q_next({});
+        for (int i = 0; i < graph.num_vertices(); ++i) {
+            Q.insert(DEdge({i, i, 0}));
+        }
+        edge_weight delta_0 = graph.get_delta_0();
+        edge_weight delta_cur = delta_0;
+        std::vector<std::vector<TEdge>> T(ceil(log(graph.get_delta_0() / delta_0)));
+        std::unordered_set<DEdge> S({}), S_next({});
+        std::multiset<DEdge> Q_dash;
+
+        for (int i = 0; i < T.size(); ++i) {
+            S_next.clear();
+            Q_next.clear();
+            for (auto t_edge : T[i]) {
+                auto block = blocked_adjacency_lists[t_edge.to][t_edge.b];
+                for (auto edge : block) {
+                    //{(u, w, x + c(v, w))};
+                    Q_dash.insert(DEdge({t_edge.from, edge.to, t_edge.weight + edge.weight}));
+                }
+                // log((x + c(first edge of block b + 1 in v’s adjacency list))/∆0)
+                // TODO: not sure how t_edge.b + 1 works here
+                double c = blocked_adjacency_lists[t_edge.to][t_edge.b + 1][0].weight;
+                int j = floor(log((t_edge.weight + c)) / delta_0);
+                T[j].push_back(TEdge({t_edge.from, t_edge.to, t_edge.weight, t_edge.b + 1}));
+            }
+
+            while (!Q.empty()) {
+                for (auto d_edge : Q) {
+                    // foreach  edge(v, w) ∈ E having c(v, w) < ∆cur do
+                    //         Q' : = Q' ∪ { (u, w, x + c(v, w)) }
+                    for (auto edge : graph.edges_from(d_edge.to)) {
+                        if (edge.weight < delta_cur) {
+                            Q_dash.insert(DEdge({d_edge.from, edge.to, d_edge.weight + edge.weight}));
+                        }
+                    }
+                }
+                // semi - sort Q' by common start and destination node -> happens automatically
+                // H:= {(u, v, x) : x = min{y : (u, v, y) ∈ Q' } }
+                edge_weight min_weight = std::numeric_limits<edge_weight>::max();
+                for (const auto &d_edge : Q_dash) {
+                    if (d_edge.weight < min_weight) {
+                        min_weight = d_edge.weight;
+                    }
+                }
+                std::unordered_set<DEdge> H;
+                for (const auto &d_edge : Q_dash) {
+                    if (d_edge.weight == min_weight) {
+                        H.insert(d_edge);
+                    }
+                }
+                for (auto &d_edge : H) {
+                    UEdge conn({d_edge.from, d_edge.to});
+                    if (d_edge.weight < delta_cur) {
+                        Q.insert(d_edge);
+
+                        if (found.get(conn) == inf) {
+                            S.insert(d_edge);
+                        }
+                    } else {
+                        Q_next.insert(d_edge);
+                        if (found.get(conn) == inf) {
+                            S_next.insert(d_edge);
+                        }
+                    }
+                    found.insert(conn, d_edge.weight);
+                }
+                Q_dash.clear();
+            }
+            for (DEdge edge : S) {
+                // b = first block in v’s adj.list having edges heavier than ∆cur
+                int b = 0;
+                bool flag = false;
+                while (!flag) {
+                    for (auto edge : blocked_adjacency_lists[edge.to][b]) {
+                        if (edge.weight > delta_cur) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                    b++;
+                }
+                edge_weight x = found.get(UEdge({edge.from, edge.to}));
+                double c = blocked_adjacency_lists[edge.to][b][0].weight;
+                int j = floor(log((x + c)) / delta_0);
+                T[j].push_back(TEdge({edge.from, edge.to, x, b}));
+            }
+            Q = Q_next;
+            S = S_next;
+            delta_cur *= 2;
+        }
+        return graph.get_delta_0();
+    }
 };
 
-int main()
-{
+int main() {
     /*
            C -> D -> I
           /           \
@@ -286,13 +347,13 @@ int main()
 
     vertex_t source = 0;
 
-    for (size_t i = 0; i < res.size(); i++)
-    {
-        if (res[i] != resPara[i])
-        {
+    for (size_t i = 0; i < res.size(); i++) {
+        if (res[i] != resPara[i]) {
             std::cout << "ERROR: " << i << ": " << res[i] << " != " << resPara[i] << std::endl;
         }
     }
+
+    printf("%d\n", solver.find_delta());
     // for (size_t i = 0; i < resPara.size(); i++) {
     //     std::cout << i << ": " << resPara[i] << std::endl;
     // }
